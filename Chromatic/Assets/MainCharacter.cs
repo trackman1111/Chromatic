5bbe496e41c18e55ca368f6d8e8d7d320751603a
Brandon Morrell <brandon@morrellworld.net>
2021-09-15 18:19:57 -0500

    Added Movement Script

---
+++ Chromatic/Assets/MainCharacter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), 0);
+        transform.Translate(move * Time.deltaTime * moveSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+        }
+    }
+}
