7c6d0889095e23e2818ab8ac622cdd0899abee32
Brandon Morrell <brandon@morrellworld.net>
2021-09-24 15:24:58 -0500

    Pushing all major edits to scene
    Added in sprites, edited circle prefab to match more realistic collisions, changed main character script to accomodate climbing, edited scene. Additionally organized files

--- Chromatic/Assets/Main Character/MainCharacter.cs
+++ Chromatic/Assets/Main Character/MainCharacter.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MainCharacter : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float moveSpeed = 9.0f;
+    float gravscale;
+    public float ClimbSpeed;
     public bool hasRed = false;
     public bool hasBlue = false;
     public bool hasYellow = false;
     public bool hasGreen = false;
+    private bool climbing;
+    public Sprite vine;
+    public Tilemap background;
+    private Vector2 move;
 
 
 
@@ -17,18 +24,41 @@ public class MainCharacter : MonoBehaviour
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        gravscale = rb.gravityScale;
+        climbing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), 0);
-        transform.Translate(move * Time.deltaTime * moveSpeed);
+        Sprite currentSprite = background.GetSprite(new Vector3Int((int)(transform.position.x), (int)transform.position.y, 0));
+        float xSpeed = Input.GetAxis("Horizontal");
+        float ySpeed = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKeyDown(KeyCode.Space) && !climbing)
         {
             rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
         }
+        if (currentSprite != null && currentSprite.Equals(vine) && !climbing)
+        {
+            climbing = true;
+            rb.gravityScale = 0;
+            rb.velocity = new Vector2(0, 0);
+        }
+        else if (climbing && (currentSprite == null || !currentSprite.Equals(vine)))
+        {
+            rb.gravityScale = 1;
+            climbing = false;
+        }
+        if (!climbing)
+        {
+            ySpeed = 0;
+        }
+        move = new Vector2(xSpeed, ySpeed);
+        transform.Translate(move * Time.deltaTime * moveSpeed);
+
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
