fc4889136031f113fabed7bc2f612baa09546fd4
Brenden Rice <rice.brenden6@gmail.com>
2021-10-15 17:40:24 -0500

    Added Flying Enemy
    Added a Flying Enemy tag to use, and made an enemy that will fly pretty organically

--- Chromatic/Assets/Enemies/Scripts/Enemy.cs
+++ Chromatic/Assets/Enemies/Scripts/Enemy.cs
@@ -14,18 +14,36 @@ public class Enemy : MonoBehaviour
     Vector3 waypointLeft;
     Vector3 moveDistanceRight; 
     Vector3 moveDistanceLeft; 
+    private Rigidbody2D rb;
+
+    // flying variables
+    private bool flying = false;
+    [Header("Adjust Flap Speed and Rigidbody gravity scale")]
+    [SerializeField] float flapSpeed = 2.1f;
 
     void Start()
     {
+        rb = gameObject.GetComponent<Rigidbody2D>();
         moveDistanceRight = new Vector3(moveXRight, 0, 0);
         moveDistanceLeft = new Vector3(moveXLeft, 0, 0);
         waypointRight = this.transform.position + moveDistanceRight;
         waypointLeft = this.transform.position - moveDistanceLeft;
+
+
+        if (gameObject.tag == "Flying Enemy")
+        {
+            flying = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y < waypointRight.y - .5f && flying)
+        {
+            rb.velocity = new Vector2(0, flapSpeed);
+        }
+
         if (transform.position.x < waypointRight.x && direction == "right")
         {
             MoveRight();
@@ -50,13 +68,13 @@ public class Enemy : MonoBehaviour
     private void MoveRight()
     {       
             var step = moveSpeed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, waypointRight, step);
+            transform.position = Vector2.MoveTowards(transform.position, new Vector3(waypointRight.x, transform.position.y, transform.position.z), step);
 
     }
 
     private void MoveLeft()
     {
             var step = moveSpeed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, waypointLeft, step);
+            transform.position = Vector2.MoveTowards(transform.position, new Vector3(waypointLeft.x, transform.position.y, transform.position.z), step);
     }
 }
